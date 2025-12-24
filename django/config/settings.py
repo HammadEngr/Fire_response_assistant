d22792016761839261ae913295e5b0870e073fd0
HammadEngr <hammadahmed.engr@gmail.com>
2025-12-24 23:25:23 +0100

    environment variables added via docker compose file

--- django/config/settings.py
+++ django/config/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o9+t3whb7gk$czs%rboj2p$g0up56*s38d4-$@&1^r-w2%d^v3'
+
+# SECRET_KEY = 'django-insecure-o9+t3whb7gk$czs%rboj2p$g0up56*s38d4-$@&1^r-w2%d^v3'
+SECRET_KEY = os.environ.get('DJANGO_SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
