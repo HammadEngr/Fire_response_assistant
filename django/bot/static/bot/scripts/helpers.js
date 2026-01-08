c7b82e17f4ecf0b01c738049563c230664170804
HammadEngr <hammadahmed.engr@gmail.com>
2026-01-08 20:01:16 +0100

    host url modified

--- django/bot/static/bot/scripts/helpers.js
+++ django/bot/static/bot/scripts/helpers.js
@@ -1,6 +1,8 @@
 export async function handleRequest(headers_content, body_content) {
   try {
-    const apiEndpont = "http://localhost/api/get_response/";
+    const apiEndpont = "/api/get_response/";
+    // const apiEndpont = "http://13.50.248.208/api/get_response/";
+    // const apiEndpont = "http://localhost/api/get_response/";
     // const apiEndpont = "http://localhost:8000/api/get_response/"
     const res = await fetch(apiEndpont, {
       method: "POST",
